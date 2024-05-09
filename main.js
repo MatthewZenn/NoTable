a3a76aa2aecfb7092fca560117f806f000f69b09
Matthew Zenn <spyguy57@gmail.com>
2024-05-09 17:52:30 -0400

    fixed bugs

--- main.js
+++ main.js
@@ -3,19 +3,21 @@ const numbers = document.getElementById("numbers");
 const output = document.getElementById('output');
 
 document.getElementById('copy').addEventListener('click', function() {
-  navigator.clipboard.writeText(document.getElementById("editor").value);
+  navigator.clipboard.writeText(document.getElementById("output").value);
 });
 
 document.getElementById('convert').addEventListener('click', function() {
   output.value = '';
   var text = textarea.value;   
   var colum2 = text.split(/\r?\n/);
-  var colum1 = colum2.splice(0, Math.ceil(colum2.length/2))
+  var colum1 = colum2.splice(0, Math.ceil(colum2.length/2));
+  console.log(colum1);
+  console.log(colum2);
   output.value = "| Lanuage | Status | Language | Status |\n|----|----|----|----|";
   for (let i=0; i<colum1.length; i++) {
-    var item1 = colum1.shift().split(',');
-    var item2 = colum2.shift().split(',');
-    output.value += "\n| "+item1[0]+" | "+item1[1]+" | "+item2[0]+" | "+item2[1]+" | ";
+    var item1 = colum1[i].split(',');
+    var item2 = colum2[i].split(',');
+    output.value += "\n| "+item1[0]+" | "+item1[1]+" | "+item2[0]+" | "+item2[1]+" |";
   }
 });
 
