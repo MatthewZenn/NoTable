5fe8f144eac3bf1945b0ef562b6449ecbe59f20f
Matthew Zenn <spyguy57@gmail.com>
2024-05-09 16:43:27 -0400

    built site

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 const textarea = document.getElementById('editor');
 const numbers = document.getElementById("numbers");
+const output = document.getElementById('output');
+
+document.getElementById('copy').addEventListener('click', function() {
+  navigator.clipboard.writeText(document.getElementById("editor").value);
+});
 
 document.getElementById('convert').addEventListener('click', function() {
+  output.value = '';
   var text = textarea.value;   
-  var lines = text.split(/\r?\n/);
-  textarea.value = lines;
-  var count = lines.length;
+  var colum2 = text.split(/\r?\n/);
+  var colum1 = colum2.splice(0, Math.ceil(colum2.length/2))
+  output.value = "| Lanuage | Status | Language | Status |\n|----|----|----|----|";
+  for (let i=0; i<colum1.length; i++) {
+    var item1 = colum1.shift().split(',');
+    var item2 = colum2.shift().split(',');
+    output.value += "\n| "+item1[0]+" | "+item1[1]+" | "+item2[0]+" | "+item2[1]+" | ";
+  }
 });
 
 textarea.addEventListener("keyup", (e) => {
