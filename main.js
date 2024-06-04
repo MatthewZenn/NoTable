5bd653e9e09bbc6b4d3b8274a81c160d4f3b5a45
Matthew Zenn <spyguy57@gmail.com>
2024-06-04 17:20:30 -0400

    Added graph for frequency analysis

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ const numbers = document.getElementById("numbers");
 const output = document.getElementById('output');
 const one = document.getElementById('field1');
 const numb = document.getElementById('field2');
+const ctx = document.getElementById('graph');
 
 var i = 0;
 var keys = [];
 var date = [];
+
+var graph = new Chart();
 
 document.getElementById('copy').addEventListener('click', function() {
   navigator.clipboard.writeText(document.getElementById("output").value);
@@ -37,24 +40,63 @@ document.getElementById('convert').addEventListener('click', function() {
 
 document.getElementById('analyze').addEventListener('click', function() {
   output.value = '';
+  date = [];
+  keys = [];
+  i = 0;
+  graph.destroy();
+
   var input = textarea.value;
+  input = String(input).toLowerCase();
   var result = [...input].reduce((acc, chr) => { 
     acc[chr] = (acc[chr] || 0) + 1;
     return acc;
   }, {});
-  
-  console.log('Result:', result)
+
   for(var key in result) {
     if (result.hasOwnProperty(key)){
       keys.push(key);
-      date.push(result[key])
+      date.push(Number(result[key])/10);
     }
   }
   for (l=0;l<keys.length;l++) {
-    output.value += keys[l] +" "+date[l]+"\n";
+    output.value += keys[l]+','+date[l]+'\n';
   }
-});
 
+  graph = new Chart(ctx, {
+    data: {
+      datasets: [{
+          type: 'bar',
+          label: one.value,
+          data: date,
+          backgroundColor: 'rgba(153, 102, 255, 0.75)',
+          borderColor: 'rgb(153, 102, 255)'
+      }, {
+          type: 'bar',
+          label: 'Average Character Weights',
+          data: [8.2,1.5,2.8,4.3,12.7,2.2,2.0,6.1,7.0,0.15,0.77,4.0,2.4,6.7,7.5,1.9,0.095,6.0,6.3,9.1,2.8,0.98,2.4,0.15,2.0,0.074],
+          backgroundColor: 'rgba(54, 162, 235, 0.75)',
+          borderColor: 'rgb(54, 162, 235)'
+      }],
+      labels: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    },
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true,
+          grid: {
+            display: false
+          },
+        },
+        x: {
+          grid: {
+            display: false,
+          }
+        }
+      }
+    }
+  });
+});
 
 textarea.addEventListener("keyup", (e) => {
   const num = e.target.value.split("\n").length;
